940b6af59a77c668c327d29392166fe90d24fe4e
arpanjoshi <rashuarpan@rashuarpans-macbook-pro.local>
2017-07-05 22:14:15 -0700

    Initial Commit

---
+++ Checklists 2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists 2017
+//
+//  Created by RashuArpan on 7/5/17.
+//  Copyright © 2017 Avin Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
